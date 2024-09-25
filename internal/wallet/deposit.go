64d85c1b0d4264c69634627b036038accc45702a
jackzwc <zhong452244751@gmail.com>
2024-09-25 21:11:19 +0800

    fix(deposit): handle unconfirmed deposit state

--- internal/wallet/deposit.go
+++ internal/wallet/deposit.go
@@ -44,19 +44,22 @@ func (w *WalletServer) depositLoop(ctx context.Context) {
 func (w *WalletServer) processConfirmedDeposit(ctx context.Context) {
 	for {
 		queues := w.state.GetDepositState().UnconfirmQueue
-		if len(queues) != 0 {
-			for _, queue := range queues {
-				tx := DepositTransaction{
-					TxHash:      queue.TxHash,
-					RawTx:       queue.RawTx,
-					EvmAddress:  queue.EvmAddr,
-					SignVersion: queue.SignVersion,
-				}
-				go w.confirmingDeposit(ctx, tx, 0)
-			}
-		} else {
+		if len(queues) == 0 {
 			time.Sleep(5 * time.Second)
+			continue
 		}
+
+		deposit := queues[0]
+		queues = queues[1:] // remove the first element
+		w.state.UpdateDepositState(queues)
+
+		tx := DepositTransaction{
+			TxHash:      deposit.TxHash,
+			RawTx:       deposit.RawTx,
+			EvmAddress:  deposit.EvmAddr,
+			SignVersion: deposit.SignVersion,
+		}
+		go w.confirmingDeposit(ctx, tx, 0)
 	}
 }
 
@@ -70,10 +73,10 @@ func (w *WalletServer) confirmingDeposit(ctx context.Context, tx DepositTransact
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			// if tx not found, retry
-			log.Info("Confirmed deposit not found, retrying...")
+			log.Info("confirmed deposit not found, retrying...")
 		} else {
 			// if tx validate or other error found, add attempt
-			log.Infof("Confirmed deposit error: %v, attempt %d retrying...", err, attempt)
+			log.Infof("confirmed deposit error: %v, attempt %d retrying...", err, attempt)
 			attempt++
 		}
 		// TODO sleep how long?
@@ -122,7 +125,7 @@ func (w *WalletServer) confirmingDeposit(ctx context.Context, tx DepositTransact
 
 		msgSignDeposit, err := newMsgSignDeposit(tx, proposer, depositKey, merkleRoot, proof, txIndex)
 		if err != nil {
-			log.Errorf("newMsgSignDeposit err: %v", err)
+			log.Errorf("NewMsgSignDeposit err: %v", err)
 			return
 		}
 		w.state.EventBus.Publish(internalstate.SigStart, *msgSignDeposit)
@@ -136,7 +139,7 @@ func (w *WalletServer) confirmingDeposit(ctx context.Context, tx DepositTransact
 		return
 	}
 
-	log.Infof("ConfirmedDeposit success, blockHeight: %v", tx.BlockHeight)
+	log.Infof("confirmed deposit success, blockHeight: %v", tx.BlockHeight)
 }
 
 func newMsgSignDeposit(tx DepositTransaction, proposer string, pubKey []byte, merkleRoot []byte, proof []byte, txIndex uint32) (*types.MsgSignDeposit, error) {
