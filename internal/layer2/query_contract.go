6904aeb0ebfc32ba8591fbfd805df3fbf196acc6
anvztor <15998375+anvztor@users.noreply.github.com>
2024-11-01 22:44:04 +0800

    chore(layer2): add call opts for evm abi calls

--- internal/layer2/query_contract.go
+++ internal/layer2/query_contract.go
@@ -1,20 +1,41 @@
 package layer2
 
 import (
+	"context"
+	"fmt"
 	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 func (lis *Layer2Listener) IsDeposit(txid [32]byte, txout uint32) (bool, error) {
-	return lis.contractBridge.IsDeposited(nil, txid, txout)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	opts := &bind.CallOpts{
+		Context: ctx,
+	}
+
+	return lis.contractBridge.IsDeposited(opts, txid, txout)
 }
 
 func (lis *Layer2Listener) GetWithdrawalSenderAddress(withdrawalID *big.Int) (common.Address, error) {
-	withdrawal, err := lis.contractBridge.Withdrawals(&bind.CallOpts{}, withdrawalID)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	opts := &bind.CallOpts{
+		Context: ctx,
+	}
+
+	withdrawal, err := lis.contractBridge.Withdrawals(opts, withdrawalID)
 	if err != nil {
-		return common.Address{}, err
+		return common.Address{}, fmt.Errorf("failed to get withdrawal info: %w", err)
+	}
+
+	if (withdrawal.Sender == common.Address{}) {
+		return common.Address{}, fmt.Errorf("invalid sender address returned")
 	}
 
 	return withdrawal.Sender, nil
