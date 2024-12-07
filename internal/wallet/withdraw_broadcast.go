88203c3c8c97bc35e5d3d997556cbc20ce311015
anvztor <15998375+anvztor@users.noreply.github.com>
2024-12-08 01:33:58 +0800

    chore(fireblocks): deal ErrRPCVerifyRejected with reverting and resigning

--- internal/wallet/withdraw_broadcast.go
+++ internal/wallet/withdraw_broadcast.go
@@ -5,7 +5,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -224,11 +223,10 @@ func (c *FireblocksClient) CheckPending(txid string, externalTxId string, update
 				switch rpcErr.Code {
 				case btcjson.ErrRPCTxAlreadyInChain:
 					return false, 0, 0, nil
+				// ErrRPCVerifyRejected indicates that transaction or block was rejected by network rules
 				case btcjson.ErrRPCVerifyRejected:
-					if strings.Contains(rpcErr.Message, "mandatory-script-verify-flag-failed") {
-						log.Warnf("Transaction signature verification failed, reverting for re-signing: %v, txid: %s", rpcErr, txid)
-						return true, 0, 0, nil
-					}
+					log.Warnf("transaction was rejected by network rules, reverting for re-signing: %v, txid: %s", rpcErr, txid)
+					return true, 0, 0, nil
 				}
 			}
 			return false, 0, 0, fmt.Errorf("send raw transaction error: %v, txid: %s", err, txid)
